a5aaba814bcad528f534b941f98cbfeb062f33ae
Shawn Kearney <shawnpk29@me.com>
2026-02-11 07:04:41 -0700

    Add routes, update seeds, update views, add css

--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,9 +1,33 @@
 FactoryBot.define do
-  factory :line_item do
-    line_item_date { nil }
-    name { "MyString" }
-    description { "MyText" }
+  factory :room_today, class: LineItem do
+    line_item_date { association :today }
+    name { "Meeting room" }
+    description { "A cozy meeting room for 10 people" }
     quantity { 1 }
-    unit_price { "9.99" }
+    unit_price { 1000 }
+  end
+
+  factory :catering_today, class: LineItem do
+    line_item_date { association :today }
+    name { "Meal tray" }
+    description { "Our delicious meal tray" }
+    quantity { 10 }
+    unit_price { 25 }
+  end
+
+  factory :room_next_week, class: LineItem do
+    line_item_date { association :next_week }
+    name { "Meeting Room" }
+    description { "A cozy meeting room for 10 people" }
+    quantity { 1 }
+    unit_price { 1000 }
+  end
+
+  factory :catering_next_week, class: LineItem do
+    line_item_date { association :next_week }
+    name { "Meal tray" }
+    description { "Our delicious meal tray" }
+    quantity { 10 }
+    unit_price { 25 }
   end
 end
