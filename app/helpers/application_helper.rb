25e174e49b4eb955c8903b339972b09da51c8999
Shawn Kearney <shawnpk29@me.com>
2026-02-11 09:48:50 -0700

    CRUD actions with turbo frames and streams

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     end
   end
 
+  def nested_dom_id(*args)
+    args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join("_")
+  end
+
   def render_turbo_stream_flash_messages
     turbo_stream.prepend "flash", partial: "layouts/flash"
   end
